c46173aefe3307ec848b3f2f7cf242b04f0f072b
hyfsy <1577975140@qq.com>
2022-09-18 15:18:49 +0800

    权重+灰度配置测试

--- alibaba/nacos-service-config/src/main/java/com/hyf/cloud/nacos/controller/RefreshController.java
+++ alibaba/nacos-service-config/src/main/java/com/hyf/cloud/nacos/controller/RefreshController.java
@@ -2,8 +2,11 @@ package com.hyf.cloud.nacos.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author baB_hyf
@@ -13,11 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RefreshController {
 
+    @Resource
+    private ConfigurableEnvironment environment;
+
     @Value("${hello:unset}")
     private String hello;
 
     @RequestMapping("hello")
     public String hello() {
-        return "hello: " + hello;
+        return "hello: " + hello + " - " + environment.getProperty("world");
     }
 }
